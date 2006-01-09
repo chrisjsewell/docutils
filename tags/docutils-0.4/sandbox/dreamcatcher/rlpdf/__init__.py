6996ec5cc8eeffa560fadcb2a5b69de6612401a4
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2006-01-09 20:43:46 +0000

    removed tag one level too high

--- tags/docutils-0.4/sandbox/dreamcatcher/rlpdf/__init__.py
+++
@@ -1,5 +0,0 @@
-# rlpdf subpackage
-
-from rlpdf import Writer
-from stylesheet import getStyleSheet
-from rltemplate import RLDocTemplate
--- tags/docutils-0.4/sandbox/paultremblay/rst_to_docbook/rst_to_docbook/location.py
+++
@@ -1,6 +0,0 @@
-
-def get_location():
-    return '/etc'
-
-
-    
