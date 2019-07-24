825d62ee1ff4e778f487642fe231ca3b89f5f5cd
engelbert gruber <936963+grubert@users.noreply.github.com>
2019-07-24 09:22:01 +0000

    Fix 366: as it is done in trunk Commit [r8294], moving the function.

--- docutils/docutils/nodes.py
+++ docutils/docutils/nodes.py
@@ -29,9 +29,6 @@ import warnings
 import types
 import unicodedata
 
-# 2019-07-23  Fix bugs#366 move import docutils.utils into function astext
-# import docutils.utils
-
 # ==============================
 #  Functional Node Base Classes
 # ==============================
@@ -323,6 +320,20 @@ def ensure_str(s):
         return s.encode('ascii', 'backslashreplace')
     return s
 
+# definition moved here from `utils` to avoid circular import dependency
+def unescape(text, restore_backslashes=False, respect_whitespace=False):
+    """
+    Return a string with nulls removed or restored to backslashes.
+    Backslash-escaped spaces are also removed.
+    """
+    # `respect_whitespace` is ignored (since introduction 2016-12-16)
+    if restore_backslashes:
+        return text.replace('\x00', '\\')
+    else:
+        for sep in ['\x00 ', '\x00\n', '\x00']:
+            text = ''.join(text.split(sep))
+        return text
+
 
 class Text(Node, reprunicode):
 
@@ -365,8 +376,7 @@ class Text(Node, reprunicode):
         return domroot.createTextNode(unicode(self))
 
     def astext(self):
-        import docutils.utils
-        return reprunicode(docutils.utils.unescape(self))
+        return reprunicode(unescape(self))
 
     # Note about __unicode__: The implementation of __unicode__ here,
     # and the one raising NotImplemented in the superclass Node had
